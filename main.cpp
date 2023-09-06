828c6cda5b0d843458a4e9bd7f05a1b27dc30eb7
2002五月女 <94036178+2002saotome@users.noreply.github.com>
2023-09-06 17:04:07 +0900

    弾発射、プレイヤーの移動、背景等

--- main.cpp
+++ main.cpp
@@ -41,11 +41,55 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 	// ゲームループで使う変数の宣言
 
-	//プレイヤー
-	int Player = 0;
+	//自機
+	int PlayerX = 0;
+	int PlayerY = 0;
+	int PlayerRadius = 0;
+	int PlayerLife = 3;
+	int PlayerSpeed = 20;
 
 	//敵
 	int Enemy[10];
+	int EnemyX = 0;
+	int EnemyY = 0;
+	int EnemyFlag = 0;
+
+	//弾
+	int PlayerBeamX = 0;
+	int PlayerBeamY = 0;
+	int PlayerBeamRadius = 0;
+	int PlayerBeamSpeed = 30;
+	int PlayerBeamFlag = 0;
+
+	//シーン
+	int scene = 0;
+
+
+	//リソース
+	
+	//プレイヤー
+	int PlayerGraphHandle;
+	PlayerGraphHandle = LoadGraph("Player.png");
+
+	//弾（プレイヤー）
+	int PlayerBombGraphHandle;
+	PlayerBombGraphHandle = LoadGraph("Bomb.png");
+
+	//敵
+	int EnemyGraphHandle;
+	EnemyGraphHandle = LoadGraph("Enemy.png");
+
+	//タイマー
+	int TimerGraphHandle;
+	TimerGraphHandle = LoadGraph("");
+
+	//タイトル
+	int TitleGraphHandle;
+	TitleGraphHandle = LoadGraph("");
+
+	//ゲーム背景
+	int GraphDrawHandle;
+	GraphDrawHandle = LoadGraph("DrawGraph.png");
 
 
 	// 最新のキーボード情報用
@@ -66,8 +110,174 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 		// 更新処理
 
+		//シーン
+		switch (scene)
+		{
+			//タイトル
+		case 0:
+			//初期化
+			//プレイヤー
+			PlayerX = 850;
+			PlayerY = 830;
+			EnemyX = 500;
+			EnemyY = 0;
+			PlayerLife = 3;
+			PlayerBeamFlag = 0;
+			PlayerBeamRadius = 16;
+			PlayerBeamX = 850;
+			PlayerBeamY = 830;
+			EnemyFlag = 1;
+
+			//敵
+
+			if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+			{
+				scene = 1;
+			}
+			break;
+
+			//ゲーム説明
+		case 1:
+			if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+			{
+				scene = 2;
+			}
+			break;
+
+			//ゲームプレイ
+		case 2:
+			//プレイヤー移動処理
+			if (keys[KEY_INPUT_RIGHT] == 1)
+			{
+				PlayerX += PlayerSpeed;
+			}
+			//左
+			if (keys[KEY_INPUT_LEFT] == 1)
+			{
+				PlayerX -= PlayerSpeed;
+			}
+			//上
+			if (keys[KEY_INPUT_UP] == 1)
+			{
+				PlayerY -= PlayerSpeed;
+			}
+			//下
+			if (keys[KEY_INPUT_DOWN] == 1)
+			{
+				PlayerY += PlayerSpeed;
+			}
+
+			//移動制限
+			//右
+			if (PlayerX > 1778)
+			{
+				PlayerX = 1778;
+			}
+
+			//左
+			if (PlayerX < 0)
+			{
+				PlayerX = 0;
+			}
+
+			//上
+			if (PlayerY < 0)
+			{
+				PlayerY = 0;
+			}
+
+			//下
+			if (PlayerY > 830)
+			{
+				PlayerY = 830;
+			}
+			
+
+			//弾の発射
+			if (keys[KEY_INPUT_SPACE] == 1 && oldkeys[KEY_INPUT_SPACE] == 0)
+			{
+
+				if (PlayerBeamFlag == 0)
+				{
+					//プレイヤーの座標を弾に代入
+					PlayerBeamX = PlayerX + 50;
+					PlayerBeamY = PlayerY - 20;
+					//弾を存在させる
+					PlayerBeamFlag = 1;
+				}
+			}
+
+			if (EnemyY < 1000)
+			{
+				EnemyY += 20;
+			}
+
+			if (EnemyY == 1000)
+			{
+				EnemyFlag = 0;
+			}
+
+			
+
+			if (PlayerBeamFlag == 1)
+			{
+				//上に移動
+				PlayerBeamY = PlayerBeamY - PlayerBeamSpeed;
+
+				//画面外なら
+				if (PlayerBeamY < -20)
+				{
+					//弾を消す
+					PlayerBeamFlag = 0;
+				}
+			}
+			break;
+		}
 
 		// 描画処理
+		//プレイヤー
+
+		switch (scene)
+		{
+			//タイトル
+		case 0:
+			DrawGraph(0, 0, TitleGraphHandle, true);
+			DrawFormatString(0, 0, GetColor(255, 0, 0), "Title", scene);
+			break;
+
+			//ゲーム説明
+		case 1:
+			DrawFormatString(0, 0, GetColor(255, 0, 0), "Explanation", scene);
+			break;
+
+			//ゲームプレイ
+		case 2:
+			//背景
+			DrawGraph(0, 0, GraphDrawHandle, true);
+			//プレイヤー
+			DrawGraph(PlayerX, PlayerY, PlayerGraphHandle, true);
+
+			if (EnemyFlag == 1)
+			{
+				//敵
+				DrawGraph(EnemyX, EnemyY, EnemyGraphHandle, true);
+			}
+
+			DrawFormatString(0, 0, GetColor(255, 0, 0), "PlayerX:%d", PlayerX);
+			DrawFormatString(0, 20, GetColor(255, 0, 0), "PlayerY:%d", PlayerY);
+			DrawFormatString(0, 40, GetColor(255, 0, 0), "PlayerBeamX:%d", PlayerBeamX);
+			DrawFormatString(0, 60, GetColor(255, 0, 0), "PlayerBeamY:%d", PlayerBeamY);
+			//ビーム(プレイヤー)
+			DrawFormatString(0, 80, GetColor(255, 0, 0), "PlayerBeamFlag:%d", PlayerBeamFlag);
+			if (PlayerBeamFlag == 1)
+			{
+				DrawGraph(PlayerBeamX, PlayerBeamY, PlayerBombGraphHandle,true);
+			}
+			break;
+		}
+
+		//敵
+		//DrawGraph(EnemyX)
 
 		//---------  ここまでにプログラムを記述  ---------//
 		// (ダブルバッファ)裏面
